6d8d4692694c747a3d7233fbb8f74a5d187425cc
Julian Dellacasa Ponce <jdellacasaponce@frba.utn.edu.ar>
2024-05-05 18:04:14 -0300

    Ejercicio 1-6. Se agrega verificación de "getchar () != EOF"

--- Capitulo1-LibroK&R/CopiaDeArchivos.c
+++ Capitulo1-LibroK&R/CopiaDeArchivos.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
-/* Copia la entrada a la salida; 2° Versión*/
+/* Copia la entrada a la salida; 3° Versión : Se le pasa el archivo: "ArchivoDeLectura"
+ por linea de comandos para que el programa procese sus caracteres ( cat ArchivoDeLectura | ./CopiaDeArchivos.out )
+ y se guarda la salida en el archivo CopiaDeArchivos_Output.txt */
 
 int main()
 {
     int c;
+    int verificacion;
 
-    
-    while ( (c = getchar()) != EOF) {
+    while ((verificacion = ((c = getchar()) != EOF)) != 0)
+    {
+
+        printf("\n La expresión \"getchar() != EOF\" es igual a: %d\n", verificacion);
+
         putchar(c);
     }
+
+    printf("\n La expresión \"getchar() != EOF\" es igual a: %d\n", verificacion);
 }
