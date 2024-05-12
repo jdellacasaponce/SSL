00ca003340085c7d551f73f06a335e20e604054a
Julian Dellacasa Ponce <jdellacasaponce@frba.utn.edu.ar>
2024-05-12 09:44:09 -0300

    Fix script de test

--- Capitulo1-LibroK&R/1.5.4 Conteo de palabras/ConteoDePalabras_Test.sh
+++ Capitulo1-LibroK&R/1.5.4 Conteo de palabras/ConteoDePalabras_Test.sh
@@ -3,7 +3,7 @@
 #Este es un script para realizar la prueba del programa, haciendo una comparación con la función de word count.
 #Primero leo un archivo de texto con muchas palabras y guardo la salida en otro archivo de texto:
 
-cat ArchivoDeLectura | ./ConteoDePalabras.out > ConteoDePalabras_Output.txt
+./ConteoDePalabras.out < ArchivoDeLectura > ConteoDePalabras_Output.txt
 
 #Luego comparo la salida con la funcion de word count (wc -w) que poseo en Linux.
 
