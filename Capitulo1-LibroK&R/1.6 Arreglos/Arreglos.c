56f07c7519e82733d2a5786e27eb0a00ffcca97b
Julian Dellacasa Ponce <jdellacasaponce@frba.utn.edu.ar>
2024-06-23 20:30:18 -0300

    Ejercicio 1.13

--- Capitulo1-LibroK&R/1.6 Arreglos/Arreglos.c
+++ Capitulo1-LibroK&R/1.6 Arreglos/Arreglos.c
@@ -1,48 +1,125 @@
 #include <stdio.h>
 
+#define IN 1
+#define OUT 0
+
 /* cuenta digitos, espacios en blancos, y otros */
 
 int main()
 {
-    int c, i, nwhite, nother;
-    int ndigit[10];
+    int c, contadorDeLongitud, i;
+    int longitudDePalabra[4];
 
-    nwhite = nother = 0;
-
-    for (i = 0; i < 10; ++i)
+    for (i = 0; i < 5; ++i)
     {
 
-        ndigit[i] = 0;
+        longitudDePalabra[i] = 0;
     }
+
+    // (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
+
+    contadorDeLongitud = 0;
 
     while ((c = getchar()) != EOF)
     {
 
-        if (c >= '0' && c <= '9')
-        {
+        if ( (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') ) {
 
-            ++ndigit[c - '0'];
+            ++contadorDeLongitud;
+
         }
 
-        else if (c == ' ' || c == '\n' || c == '\t')
-        {
+        else {
 
-            ++nwhite;
+            switch(contadorDeLongitud) {
+
+                case 0: {
+                    break;
+                }
+                case 1:
+                case 2: {
+                    // Aumento el contador de palabras con longitud menor a 3
+                    ++longitudDePalabra[0];
+                    contadorDeLongitud = 0;
+                    break;
+                }
+                
+                case 3:
+                case 4:
+                case 5: {
+                    // Aumento el contador de palabras con longitud de 3 a 5
+                    ++longitudDePalabra[1];
+                    contadorDeLongitud = 0;
+                    break;
+                }
+
+                case 6:
+                case 7: {
+                    //Aumento el contador de palabras con longitud de 6 o 7
+                    ++longitudDePalabra[2];
+                    contadorDeLongitud = 0;
+                    break;
+                }
+
+                case 8:
+                case 9: {
+                    //Aumento el contador de palabras con longitud de 8 o 9
+                    ++longitudDePalabra[3];
+                    contadorDeLongitud = 0;
+                    break;
+                }
+
+                default: {
+                    //Aumento el contador de palabras con longitud mayor a 9
+                    ++longitudDePalabra[4];
+                   // printf("Mayores a 9 son: %d \n", longitudDePalabra[4]);
+                    contadorDeLongitud = 0;
+                    break;
+                }
+
+
+            }
         }
 
-        else
-        {
-            ++nother;
-        }
+
     }
 
-    printf("digitos = ");
+    for (i = 0; i < 5; ++i) {
 
-    for (i = 0; i < 10; i++)
-    {
+        int j;
 
-        printf(" %d", ndigit[i]);
+       // printf("El contador es %d \n", longitudDePalabra[i]);
+
+        switch(i) {
+
+            case 0: {
+                printf("[Menores a 3]");
+                break;
+            }
+            case 1: {
+                printf("[Entre 3 a 5]");
+                break;
+            }
+            case 2: {
+                printf("[Entre 6 o 7]");
+                break;
+            }
+            case 3: {
+                printf("[Entre 8 o 9]");
+                break;
+            }
+            case 4: {
+                printf("[Mayores a 9]");
+                break;
+            }
+        }
+
+        for (j = 0; j < longitudDePalabra[i] ; ++j ) {
+            printf("|");            
+        }
+
+        putchar('\n');
     }
 
-    printf(", espacios en blancos = %d, otros = %d \n", nwhite, nother);
+
 }
