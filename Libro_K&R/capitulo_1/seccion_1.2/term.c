a15592f96deabd51b4c32d5c10814e797581c5e2
Julian Dellacasa Ponce <jdellacasaponce@frba.utn.edu.ar>
2025-05-11 01:07:47 -0300

    Ejercicio 1-3
    Agrego encabezados a la tabla de temperaturas

--- Libro_K&R/capitulo_1/seccion_1.2/term.c
+++ Libro_K&R/capitulo_1/seccion_1.2/term.c
@@ -7,13 +7,15 @@ int main(void) {
   float fahr, celsius;
   int lower, upper, step;
 
+  lower = 0;
+  upper = 300;
   step = 20; /* tamaño del incremento */
 
   fahr = lower;
 
+  printf("%8s %8s\n", "Celcius", "Farenheit");
   while (fahr <= upper) {
     celsius = (5.0 / 9.0) * (fahr - 32.0);
-    printf("%8s %8s", "Celcius", "Farenheit");
     printf("%8.0f %8.1f\n", fahr, celsius);
     fahr = fahr + step;
   }
