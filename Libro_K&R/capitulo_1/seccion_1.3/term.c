7a9fff261266567a8c0f12c07ad397eb6f38e386
Julian Dellacasa Ponce <jdellacasaponce@frba.utn.edu.ar>
2025-05-11 22:26:06 -0300

    Ejercicio 1-5

---
+++ Libro_K&R/capitulo_1/seccion_1.3/term.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* Imprime la tabla Farenheit-Celcius
+ * para fahr = 0,20,...,300 */
+
+int main(void) {
+  float fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20; /* tamaño del incremento */
+
+  celsius = lower;
+
+  printf("%8s %8s\n", "Celcius", "Farenheit");
+  for (celsius = 300; celsius >= 0; celsius = celsius - step) {
+    fahr = ((9.0 / 5.0) * celsius) + 32.0;
+    printf("%8.0f %8.0f\n", celsius, fahr);
+  }
+}
