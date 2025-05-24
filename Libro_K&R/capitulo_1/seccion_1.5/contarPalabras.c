71b54946f27c80545fc1a7e86a7406f8ce2e11c8
Julian Dellacasa Ponce <jdellacasaponce@frba.utn.edu.ar>
2025-05-24 17:50:43 -0300

    Ejercicio 1-12
    El programa ahora imprime una palabra por línea.

--- Libro_K&R/capitulo_1/seccion_1.5/contarPalabras.c
+++ Libro_K&R/capitulo_1/seccion_1.5/contarPalabras.c
@@ -1,29 +1,14 @@
 #include <stdio.h>
 
-#define IN 1  // en una palabra
-#define OUT 0 // fuera de una palabra
-
 int main() {
-  int c, nuevaLinea, nuevaPalabra, nuevoCaracter, estado;
-
-  estado = OUT;
-  nuevaLinea = nuevaPalabra = nuevoCaracter = 0;
+  int c;
 
   while ((c = getchar()) != EOF) {
 
-    ++nuevoCaracter;
-    if (c == '\n') {
-      ++nuevaLinea;
-    }
-    if (c == 32 || c == '\n' || c == '\t') {
-      estado = OUT;
-    } else if (estado == OUT) {
-      estado = IN;
-      ++nuevaPalabra;
+    if (c == 32 || c == 10 || c == 9) {
+      putchar('\n');
+    } else {
+      putchar(c);
     }
   }
-
-  printf("%s %d \n", "La cantidad de caracteres es:", nuevoCaracter);
-  printf("%s %d \n", "La cantidad de líneas es:", nuevaLinea);
-  printf("%s %d \n", "La cantidad de palabras es:", nuevaPalabra);
 }
