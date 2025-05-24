1f4751b33e66185bfb6a7ba032bf8ddf15917a79
Julian Dellacasa Ponce <jdellacasaponce@frba.utn.edu.ar>
2025-05-24 16:19:51 -0300

    Ejercicio 1-10
    Reemplazo las tabulaciones por "\t" y las barras invertidas por "\\"

--- Libro_K&R/capitulo_1/seccion_1.5/contarCaracter.c
+++ Libro_K&R/capitulo_1/seccion_1.5/contarCaracter.c
@@ -5,13 +5,13 @@ int main() {
   int caracterAnterior = 32;
 
   while ((c = getchar()) != EOF) {
-    if (c == 32 && caracterAnterior != c && caracterAnterior != 10) {
-      putchar(c);
-    } else if (c == 10 && caracterAnterior != c) {
-      putchar(c);
-    } else if (c != 9 && c != 32 && c != 10) {
+
+    if (c == 9) {
+      printf("%s", "\\t");
+    } else if (c == '\\') {
+      printf("%s", "\\\\");
+    } else {
       putchar(c);
     }
-    caracterAnterior = c;
   }
 }
