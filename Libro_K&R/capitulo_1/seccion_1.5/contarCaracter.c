29ca7c88e6336d89495637186a7d4583019c1999
Julian Dellacasa Ponce <jdellacasaponce@frba.utn.edu.ar>
2025-05-24 16:10:03 -0300

    Ejercicio 1-9
    Le quito al texto, doble espacios en blanco, doble saltos de línea,
    tabulaciones, y espacios en blanco al comienzo de una línea.

--- Libro_K&R/capitulo_1/seccion_1.5/contarCaracter.c
+++ Libro_K&R/capitulo_1/seccion_1.5/contarCaracter.c
@@ -2,23 +2,16 @@
 
 int main() {
   int c;
-  int nuevaLinea = 0;
-  int espacioEnBlanco = 0;
-  int tabulacion = 0;
+  int caracterAnterior = 32;
 
   while ((c = getchar()) != EOF) {
-
-    if (c == 10) {
-      ++nuevaLinea;
+    if (c == 32 && caracterAnterior != c && caracterAnterior != 10) {
+      putchar(c);
+    } else if (c == 10 && caracterAnterior != c) {
+      putchar(c);
+    } else if (c != 9 && c != 32 && c != 10) {
+      putchar(c);
     }
-    if (c == 32) {
-      ++espacioEnBlanco;
-    }
-    if (c == 9) {
-      ++tabulacion;
-    }
+    caracterAnterior = c;
   }
-  printf("La cantidad de caracteres de nueva línea son: %d \n", nuevaLinea);
-  printf("Los espacio en blanco son: %d \n", espacioEnBlanco);
-  printf("La cantidad de caracteres de tabulación son: %d \n", tabulacion);
 }
